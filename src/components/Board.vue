a0ba453a0c565206847ff138ff0d247e34a84deb
Loren Mariquit <l.mariquit@gmail.com>
2019-05-21 19:36:49 -0400

    loops through board to create columns. No style

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,7 +1,24 @@
-<template></template>
+<template>
+  <div>
+    <div v-for="column in board">
+      <Column v-bind:numbers="column"/>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {}
+import Column from './Column.vue'
+export default {
+  name: 'Board',
+  components: {
+    Column
+  },
+  data() {
+    return {
+      board: [[1, 2, 3], [4, 5, 6]]
+    }
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Column.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-for="number in numbers" v-bind:key="number">{{number}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Column',
+  props: ['numbers']
+}
+</script>
+
+<style>
+</style>
